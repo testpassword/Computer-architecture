4054d5387508e9af3a84d9bb92c2a69a85b940b6
Artemy <kulbakowork@mail.ru>
2020-10-23 13:08:30 +0300

    РІС‹РїРѕР»РЅРµРЅР° Р»Р°Р±5

--- lab5-22.10.20/program.c
+++ lab5-22.10.20/program.c
@@ -1,5 +1,16 @@
 #include <reg51.h>
 #include <math.h>
 
+char bdata mem;
+sbit x1 = mem^0;
+sbit x2 = mem^1;
+sbit y1 = mem^2;
+sbit y2 = mem^3;
+sbit z = P1^0;
+
 void main() {
+	for (mem = 0; mem < 8; mem++) {
+		P1 <<= 1;
+		z = (!x1 | y1) & (x2 & y2 | !y1);
+	}
 }
